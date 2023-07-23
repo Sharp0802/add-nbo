9e2c2ce837706a7c8a9176a17e91c66f7d80a9fb
Yeong-won Seo <sharp0802.business@gmail.com>
2023-07-23 15:40:07 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,16 +5,25 @@
 #include <uchar.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
 
-#define char8_t int8_t
+typedef int8_t char8_t;
 
-uint32_t ru32(char8_t* fpath)
+bool ru32(const char8_t* fpath, uint32_t* v)
 {
 	uint32_t fdat;
+	struct stat st;
 	FILE* fp = fopen(fpath, "rb");
+	fstat(fp, &st);
+	if (st.st_size < 4)
+	{
+		fclose(fp);
+		return false;
+	}
 	fread(&fdat, sizeof(uint32_t), 1, fp);
 	fclose(fp);
-	return ntohl(fdat);
+	*v = ntohl(fdat);
+	return true;
 }
 
 int32_t main(int32_t argc, char8_t *argv[])
@@ -22,10 +31,12 @@ int32_t main(int32_t argc, char8_t *argv[])
 	if (argc != 3)
 		return 1;
 
-	uint32_t u_0 = ru32(argv[1]);
-	uint32_t u_1 = ru32(argv[2]);
+	uint32_t u_0;
+	uint32_t u_1;
+	if (!ru32(argv[1], &u_0) || !ru32(argv[2], &u_1))
+		return 1;
 
-	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)", u_0, u_0, u_1, u_1, u_0 + u_1, u_0 + u_1);
+	printf("%u(0x%x) + %u(0x%x) = %u(0x%x)", u_0, u_0, u_1, u_1, u_0 + u_1, u_0 + u_1);
 	return 0;
 }
 
